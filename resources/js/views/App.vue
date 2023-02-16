04351ef51e2c445237768fd3538605bef24c45cb
alessandrolega <alessandrolega24@gmail.com>
2023-02-16 15:34:54 +0100

    test routes e divisione front - back office

---
+++ resources/js/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Home Guest</h1>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name:"App",
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/', 'HomeController@index')->name('index');
 });
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where("any", ".*");
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
